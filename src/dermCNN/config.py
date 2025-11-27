cf599f4341ca7c980c3630673b20f5c1b01d5f23
Nestivi <mkr.kul2003@gmail.com>
2025-11-27 23:26:52 +0100

    feat: add global configuration for paths, image size and batch size

---
+++ src/dermCNN/config.py
@@ -0,0 +1,10 @@
+# config.py
+
+BASE_DIR = r"C:\Martin\Studia\Praca Licencjacka\test\tests\ISIC_2019_Training_Input\ISIC_2019_Training_Input"
+CSV_PATH = r"C:\Martin\Studia\Praca Licencjacka\test\tests\ISIC_2019_Training_GroundTruth.csv"
+
+IMG_SIZE = 224
+BATCH_SIZE = 32
+EPOCHS = 20
+
+MODEL_OUTPUT_PATH = "results/model_isic_cnn.h5"
