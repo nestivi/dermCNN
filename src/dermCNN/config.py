40719ab4656893312642b101ad973ffd71e95fb1
Nestivi <mkr.kul2003@gmail.com>
2025-11-28 01:37:13 +0100

    fix: set correct BASE_DIR path

--- src/dermCNN/config.py
+++ src/dermCNN/config.py
@@ -6,7 +6,7 @@ import os
 BASE_PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "tests"))
 
 # Paths to data
-BASE_DIR = os.path.join(BASE_PROJECT_DIR, "ISIC_2019_Training_Input", "ISIC_2019_Training_Input")
+BASE_DIR = os.path.join(BASE_PROJECT_DIR, "ISIC_2019_Training_Input")
 CSV_PATH = os.path.join(BASE_PROJECT_DIR, "ISIC_2019_Training_GroundTruth.csv")
 
 # Model parameters
