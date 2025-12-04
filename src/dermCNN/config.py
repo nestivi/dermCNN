75173372eae192311fb8226a3e56296f7c226909
Nestivi <mkr.kul2003@gmail.com>
2025-12-04 12:34:39 +0100

    fix: add cleaner way of finding paths

--- src/dermCNN/config.py
+++ src/dermCNN/config.py
@@ -3,11 +3,11 @@ import os
 # config.py
 
 # Path to the base project directory
-BASE_PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "tests"))
+BASE_PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 # Paths to data
-BASE_DIR = os.path.join(BASE_PROJECT_DIR, "ISIC_2019_Training_Input", "ISIC_2019_Training_Input")
-CSV_PATH = os.path.join(BASE_PROJECT_DIR, "ISIC_2019_Training_GroundTruth.csv")
+BASE_DIR = os.path.join(BASE_PROJECT_DIR, "data", "ISIC_2019_Training_Input")
+CSV_PATH = os.path.join(BASE_PROJECT_DIR, "data", "ISIC_2019_Training_GroundTruth.csv")
 
 # Model parameters
 IMG_SIZE = 224
@@ -15,4 +15,6 @@ BATCH_SIZE = 32
 EPOCHS = 20
 
 # Model output path
+RESULTS_PATH = os.path.join(BASE_PROJECT_DIR, "results")
+os.makedirs(RESULTS_PATH, exist_ok=True)
 MODEL_OUTPUT_PATH = os.path.join(BASE_PROJECT_DIR, "results", "model_isic_cnn.h5")
