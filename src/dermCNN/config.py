afb2f81c003eceabe9cfa81d4f5668c7ee4a0f3d
Nestivi <mkr.kul2003@gmail.com>
2026-02-19 01:46:26 +0100

    fix: update model parameters and output path in config

--- src/dermCNN/config.py
+++ src/dermCNN/config.py
@@ -1,6 +1,5 @@
-import os
-
 # config.py
+import os
 
 # Path to the base project directory
 BASE_PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "tests"))
@@ -12,7 +11,12 @@ CSV_PATH = os.path.join(BASE_PROJECT_DIR, "ISIC_2019_Training_GroundTruth.csv")
 # Model parameters
 IMG_SIZE = 224
 BATCH_SIZE = 32
-EPOCHS = 20
+EPOCHS = 5
+
+# Class list - CSV ISIC 2019
+CLASSES = ['MEL', 'NV', 'BCC', 'AK', 'BKL', 'DF', 'VASC', 'SCC']
+NUM_CLASSES = len(CLASSES)
 
 # Model output path
-MODEL_OUTPUT_PATH = os.path.join(BASE_PROJECT_DIR, "results", "model_isic_cnn.h5")
+# MODEL_OUTPUT_PATH = os.path.join(BASE_PROJECT_DIR, "results", "model_isic_efficientnet.h5")
+MODEL_OUTPUT_PATH = os.path.join("results", "model_isic_efficientnet.keras") # Zmiana rozszerzenia
