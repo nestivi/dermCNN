b000e2aaf986d66655709c80de4181dbffe42c11
Nestivi <mkr.kul2003@gmail.com>
2026-02-23 10:19:01 +0100

    feat: config updated to new step based running of program - setting new two lists: benign and malignant for binary classification in step 1

--- src/dermCNN/config.py
+++ src/dermCNN/config.py
@@ -1,22 +1,22 @@
-# config.py
+# onfig.py
 import os
 
-# Path to the base project directory
 BASE_PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "tests"))
-
-# Paths to data
 BASE_DIR = os.path.join(BASE_PROJECT_DIR, "ISIC_2019_Training_Input")
 CSV_PATH = os.path.join(BASE_PROJECT_DIR, "ISIC_2019_Training_GroundTruth.csv")
 
-# Model parameters
 IMG_SIZE = 224
 BATCH_SIZE = 32
-EPOCHS = 5
+EPOCHS = 20
 
-# Class list - CSV ISIC 2019
+# Lista wszystkich oryginalnych klas
 CLASSES = ['MEL', 'NV', 'BCC', 'AK', 'BKL', 'DF', 'VASC', 'SCC']
-NUM_CLASSES = len(CLASSES)
 
-# Model output path
-# MODEL_OUTPUT_PATH = os.path.join(BASE_PROJECT_DIR, "results", "model_isic_efficientnet.h5")
-MODEL_OUTPUT_PATH = os.path.join("results", "model_isic_efficientnet.keras") # Zmiana rozszerzenia
+# NOWE: Podział na łagodne i złośliwe
+BENIGN_CLASSES = ['NV', 'BKL', 'DF', 'VASC']
+MALIGNANT_CLASSES = ['MEL', 'BCC', 'AK', 'SCC']
+
+# Ścieżka zapisu dla pierwszego modelu (Etap 1)
+MODEL_OUTPUT_PATH = os.path.join(BASE_PROJECT_DIR, "results", "model_stage1_binary.keras")
+
+# MODEL_OUTPUT_PATH = os.path.join("results", "model_isic_efficientnet.keras") # Zmiana rozszerzenia
