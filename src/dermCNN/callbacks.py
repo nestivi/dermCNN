825d5a5b33b2b864c5c20a0321736596e33b4567
Nestivi <mkr.kul2003@gmail.com>
2026-02-26 02:14:07 +0100

    feat: update get_callbacks function to accept mode parameter for dynamic model and log file naming

--- src/dermCNN/callbacks.py
+++ src/dermCNN/callbacks.py
@@ -1,20 +1,20 @@
-# callbacks.py
-
+# src/dermCNN/callbacks.py
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger
 
-def get_callbacks():
+def get_callbacks(mode='binary'):
     early = EarlyStopping(
         monitor="val_loss",
         patience=5,
         restore_best_weights=True
     )
 
+    # Zapisze np. 'best_model_binary.keras' lub 'best_model_malignant_only.keras'
     checkpoint = ModelCheckpoint(
-        "results/best_model.h5",
+        f"results/best_model_{mode}.keras",
         monitor="val_loss",
         save_best_only=True
     )
 
-    logger = CSVLogger("results/training_log.csv")
+    logger = CSVLogger(f"results/training_log_{mode}.csv")
 
     return [early, checkpoint, logger]
